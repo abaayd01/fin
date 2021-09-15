a7c0f8e70ac5480259c993f1181f489ba24e704e
Ayden Aba <ayden@codefishstudio.com>
2021-09-15 21:32:56 +0930

    setup jetty http server in core.clj, wire it up to component
    - also sorted required namespaces, to improve parse-ability

--- src/fin/core.clj
+++ src/fin/core.clj
@@ -1,25 +1,58 @@
 (ns fin.core
   (:gen-class)
   (:require
+    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+    [com.stuartsierra.component :as component]
     [fin.components.db :refer [make-db]]
     [fin.components.malli-instrumenter :refer [make-malli-instrumenter]]
     [fin.components.transaction-repository :refer [make-transaction-repository]]
-
-    [fin.protocols :as p]
-
-    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-    [com.stuartsierra.component :as component]
-    [next.jdbc.connection :as connection])
+    [fin.middleware :as middleware]
+    [next.jdbc.connection :as connection]
+    [reitit.ring :as ring]
+    [ring.adapter.jetty :as jetty])
   (:import (com.zaxxer.hikari HikariDataSource)))
+
+(defn handler [req]
+  {:status 200, :body {:hello "world!"}})
+
+(defn post-handler [req]
+  {:status 200, :body {:hello "world!"}})
+
+(def muuntaja-instance
+  (muuntaja.core/create))
+
+(def app (ring/ring-handler
+           (ring/router
+             ["/api"
+              {:muuntaja   muuntaja-instance
+               :middleware middleware/pipeline}
+              ["/ping" {:get  handler
+                        :post post-handler
+                        :name ::ping}]])
+           (ring/routes
+             (ring/create-default-handler))))
+
+(defrecord Server [handler options]
+  component/Lifecycle
+  (start [this]
+    (assoc this :server (jetty/run-jetty handler options)))
+
+  (stop [this]
+    (.stop (:server this))
+    (assoc this :server nil)))
+
+(defn make-server [handler options]
+  (->Server handler options))
 
 (def ^:private db-spec {:dbtype "mysql" :dbname "fin" :username "root" :password "password"})
 
-(defn create-system [{:keys [db-spec]}]
+(defn create-system [{:keys [db-spec server-options handler]}]
   (-> (component/system-map
         :ds (connection/component HikariDataSource db-spec)
         :db (make-db)
         :transaction-repository (make-transaction-repository :transactions)
-        :malli-instrumenter (make-malli-instrumenter))
+        :malli-instrumenter (make-malli-instrumenter)
+        :server (make-server handler server-options))
       (component/system-using
         {:db                     {:ds :ds}
          :transaction-repository {:db :db}})))
@@ -27,7 +60,11 @@
 (defonce the-system nil)
 
 (defn init []
-  (alter-var-root #'the-system (constantly (create-system {:db-spec db-spec}))))
+  (alter-var-root #'the-system (constantly
+                                 (create-system {:db-spec        db-spec
+                                                 :server-options {:port  3000
+                                                                  :join? false}
+                                                 :handler        app}))))
 
 (defn start []
   (alter-var-root #'the-system component/start))
